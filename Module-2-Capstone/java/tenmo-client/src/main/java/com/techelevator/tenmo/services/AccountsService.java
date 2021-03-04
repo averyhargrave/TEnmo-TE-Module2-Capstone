7d2a624178501245d9a383a02b320e9a7f2e3893
Avery Hargrave <avery.s.hargrave@gmail.com>
2021-03-04 11:26:25 -0500

    this is a mess

--- Module-2-Capstone/java/tenmo-client/src/main/java/com/techelevator/tenmo/services/AccountsService.java
+++ Module-2-Capstone/java/tenmo-client/src/main/java/com/techelevator/tenmo/services/AccountsService.java
@@ -1,5 +1,38 @@
 package com.techelevator.tenmo.services;
 
+import java.math.BigDecimal;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.client.RestTemplate;
+
+import com.techelevator.tenmo.models.AuthenticatedUser;
+
 public class AccountsService {
 
+	private String BASE_URL;
+	private RestTemplate restTemplate = new RestTemplate();
+	private AuthenticatedUser currentUser;
+	
+	public AccountsService(String url, AuthenticatedUser currentUser) {
+		this.currentUser = currentUser;
+		BASE_URL = url;
+	}
+	
+	public BigDecimal getBalance() {
+		BigDecimal bal = new BigDecimal(0);
+		bal = restTemplate.exchange(BASE_URL + "balance/" + currentUser.getUser().getId(), HttpMethod.GET, makeAuthEntity(), BigDecimal.class).getBody();
+		System.out.println("Your current account balance is: $" + bal);
+		return bal;
+	}
+	
+	 private HttpEntity makeAuthEntity() {
+	    HttpHeaders headers = new HttpHeaders();		
+	    headers.setBearerAuth(currentUser.getToken());
+	    HttpEntity entity = new HttpEntity<>(headers);
+	    return entity;
+	}
+	  
 }
+
